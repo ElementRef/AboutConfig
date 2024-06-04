2105a42205c36cb6f32c47957130287f798ded5a
ElementRef <168389155+ElementRef@users.noreply.github.com>
2024-06-04 08:02:27 +0000

    更新分流重写

--- script/app2smile.spotify.json.js
+++ script/app2smile.spotify.json.js
@@ -1,2 +1,2 @@
 // https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.json.js
-let url=$request.url;url.includes("platform=iphone")&&(url=url.replace(/platform=iphone/,"platform=ipad")),$done({url});
+let url=$request.url;url.includes("platform=iphone")&&(url=url.replace(/platform=iphone/,"platform=ipad"));$done({url});
