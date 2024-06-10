2e68f31ac483d772f493ae7d544d0200b76575df
ElementRef <168389155+ElementRef@users.noreply.github.com>
2024-06-10 20:37:14 +0800

    更新分流脚本

--- conf/generate.filter.mjs
+++ conf/generate.filter.mjs
@@ -164,9 +164,15 @@ async function getResourses({ FILENAME, SRC, MAPFN }) {
 }
 function mapMixture(text) {
   const textTemp = text.trim();
-  const textPure = (textTemp.split(',')[1] || '')
-    .replace(/\/\/.*/gim, '')
-    .trim();
+  let textPure = (textTemp.split(',')[1] || '').replace(/\/\/.*/gim, '').trim();
+  if (textPure.endsWith('.ingest.sentry.io')) {
+    /**
+     * 123456789.ingest.sentry.io
+     * asdfghjkl.ingest.sentry.io
+     * 保留 ingest.sentry.io 即可
+     */
+    textPure = 'ingest.sentry.io';
+  }
   // Quantumult X 似乎不支持 DOMAIN|RULE-SET/PROCESS-NAME
   if (textTemp.startsWith('.')) {
     return `HOST-SUFFIX,${textTemp.substring(1)}`;
