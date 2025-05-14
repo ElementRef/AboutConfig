6eafcebac545761870dff6e1dcc6f1e068c78306
ElementRef <168389155+ElementRef@users.noreply.github.com>
2025-05-14 22:09:26 +0800

    测试环境变量

--- conf/generate.blackmatrix7.mjs
+++ conf/generate.blackmatrix7.mjs
@@ -1,6 +1,7 @@
 import { writeFile } from 'node:fs/promises';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+console.log(process.env);
 const RESOURCES = {
   'blackmatrix7.rewrite.conf':
     'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rewrite/QuantumultX/Advertising/Advertising.conf'
