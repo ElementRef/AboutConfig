32cdc12751113e754370af360d74142a16db4593
ElementRef <168389155+ElementRef@users.noreply.github.com>
2025-06-07 21:20:05 +0000

    更新分流重写

--- script/app2smile.spotify.json.js
+++ script/app2smile.spotify.json.js
@@ -1,2 +1,2 @@
-// Fri Jun 06 2025 21:21:21 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.json.js
+// Sat Jun 07 2025 21:19:41 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.json.js
 let url=$request.url;url.includes("platform=iphone")&&(url=url.replace(/platform=iphone/,"platform=ipad"));$done({url});
--- script/ddgksf2013.startup.v2ex.js
+++ script/ddgksf2013.startup.v2ex.js
@@ -1,4 +1,4 @@
-// Fri Jun 06 2025 21:21:21 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
+// Sat Jun 07 2025 21:19:41 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
 var body=$response.body.replace(/<head>/,`<head>
     <style>
       .sidebar_units,
