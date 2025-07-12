8f720e6074212a4401e5585c51a3772476f865e3
ElementRef <168389155+ElementRef@users.noreply.github.com>
2025-07-12 21:21:14 +0000

    更新分流重写

--- script/ddgksf2013.startup.jd.js
+++ script/ddgksf2013.startup.jd.js
@@ -1,2 +1,2 @@
-// Fri Jul 11 2025 21:22:23 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.jd.js
+// Sat Jul 12 2025 21:20:51 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.jd.js
 var puimCea1=JSON.parse($response.body);$request.url.indexOf("hotWords")!==-1&&(puimCea1.hotwords={},puimCea1.tabs={},delete puimCea1.abver);$request.url.indexOf("hotSearchTerms")!==-1&&(puimCea1.topData.data={},puimCea1.data={});$done({body:JSON.stringify(puimCea1)});
