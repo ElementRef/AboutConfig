206ce75aaa0ed4d7258f324f49a76ad6749eda12
ElementRef <168389155+ElementRef@users.noreply.github.com>
2025-07-13 21:21:35 +0000

    更新分流重写

--- script/ddgksf2013.amdc.js
+++ script/ddgksf2013.amdc.js
@@ -1,2 +1,2 @@
-// Sat Jul 12 2025 21:20:51 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.amdc.js
+// Sun Jul 13 2025 21:21:09 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.amdc.js
 var ua=$request.headers["User-Agent"]||$request.headers["user-agent"];/(AMap|Cainiao|%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C|%E5%96%B5%E8%A1%97|%E5%A4%A9%E7%8C%AB|Alibaba|Hema4iPhone|Moon|DMPortal)/.test(ua)?$done({body:"ddgksf2013"}):$done({});
--- script/ddgksf2013.startup.umetrip.js
+++ script/ddgksf2013.startup.umetrip.js
@@ -1,2 +1,2 @@
-// Sat Jul 12 2025 21:20:51 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.umetrip.js
+// Sun Jul 13 2025 21:21:09 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.umetrip.js
 var ua=$request.headers.rpid||$request.headers.Rpid;ua.includes("10000012")||ua.includes("1000019")?$done({status:"HTTP/1.1 404 Not Found"}):$done({});
