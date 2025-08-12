62c7ee1645a23df0d31a001d95d4a0ce90477650
ElementRef <168389155+ElementRef@users.noreply.github.com>
2025-08-12 13:22:10 +0000

    更新分流重写

--- script/ddgksf2013.startup.v2ex.js
+++ script/ddgksf2013.startup.v2ex.js
@@ -1,4 +1,4 @@
-// Mon Aug 11 2025 21:20:25 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
+// Tue Aug 12 2025 13:21:45 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
 var body=$response.body.replace(/<head>/,`<head>
     <style>
       .sidebar_units,
