88dcba1ef733ad5907cd8024b3f6bec988cc0d4e
ElementRef <168389155+ElementRef@users.noreply.github.com>
2025-08-18 21:20:25 +0000

    更新分流重写

--- script/ddgksf2013.startup.umetrip.js
+++ script/ddgksf2013.startup.umetrip.js
@@ -1,2 +1,2 @@
-// Mon Aug 18 2025 10:34:11 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.umetrip.js
+// Mon Aug 18 2025 21:20:03 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.umetrip.js
 var ua=$request.headers.rpid||$request.headers.Rpid;ua.includes("10000012")||ua.includes("1000019")?$done({status:"HTTP/1.1 404 Not Found"}):$done({});
--- script/ddgksf2013.startup.v2ex.js
+++ script/ddgksf2013.startup.v2ex.js
@@ -1,4 +1,4 @@
-// Mon Aug 18 2025 10:34:11 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
+// Mon Aug 18 2025 21:20:03 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
 var body=$response.body.replace(/<head>/,`<head>
     <style>
       .sidebar_units,
