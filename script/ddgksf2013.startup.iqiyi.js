efec434e7452aaba6e75692e060a7bdf9a462c3e
ElementRef <168389155+ElementRef@users.noreply.github.com>
2025-09-07 21:18:12 +0000

    更新分流重写

--- script/ddgksf2013.startup.iqiyi.js
+++ script/ddgksf2013.startup.iqiyi.js
@@ -1,2 +1,2 @@
-// Sat Sep 06 2025 21:17:38 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
+// Sun Sep 07 2025 21:17:44 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
 let obj=JSON.parse($response.body);delete obj.adSlots,$done({body:JSON.stringify(obj)});
--- script/ddgksf2013.startup.v2ex.js
+++ script/ddgksf2013.startup.v2ex.js
@@ -1,4 +1,4 @@
-// Sat Sep 06 2025 21:17:38 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
+// Sun Sep 07 2025 21:17:44 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
 var body=$response.body.replace(/<head>/,`<head>
     <style>
       .sidebar_units,
