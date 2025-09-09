b558d81ad2e8b959172e4d9bd023d839467c33d5
ElementRef <168389155+ElementRef@users.noreply.github.com>
2025-09-09 21:18:59 +0000

    更新分流重写

--- script/ddgksf2013.startup.sf.js
+++ script/ddgksf2013.startup.sf.js
@@ -1,2 +1,2 @@
-// Tue Sep 09 2025 12:37:57 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.sf.js
+// Tue Sep 09 2025 21:18:24 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.sf.js
 if($request.url.indexOf("app/ad/queryInfoFlow")!=-1){var ddgksf2013=JSON.parse($response.body);ddgksf2013.obj=Object.values(ddgksf2013.obj).filter(e=>e.adverId==2833),$done({body:JSON.stringify(ddgksf2013)})}
