018608d7ec3e39d09a487796e13ce8820323ebbb
ElementRef <168389155+ElementRef@users.noreply.github.com>
2025-11-21 21:20:55 +0000

    更新分流重写

--- script/ddgksf2013.startup.iqiyi.js
+++ script/ddgksf2013.startup.iqiyi.js
@@ -1,2 +1,2 @@
-// Thu Nov 20 2025 21:21:13 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
+// Fri Nov 21 2025 21:20:29 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
 let obj=JSON.parse($response.body);delete obj.adSlots,$done({body:JSON.stringify(obj)});
--- script/fmz200.baidu.lib.js
+++ script/fmz200.baidu.lib.js
@@ -1,2 +1,2 @@
-// Thu Nov 20 2025 21:21:13 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/fmz200.baidu.lib.js
+// Fri Nov 21 2025 21:20:29 GMT+0000 (Coordinated Universal Time) https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/fmz200.baidu.lib.js
 let obj=JSON.parse($response.body);obj.data.vip.base_vip_info={uid:12345678,type:2,start_time:1622222200,end_time:4622222200,is_vip:1,remain_day:999,pro_total:5,normal_total:5};$done({body:JSON.stringify(obj)});
