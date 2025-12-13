313d6dbba3fb6cb7a7bb10f90f1de1e4dc36a0f1
ElementRef <168389155+ElementRef@users.noreply.github.com>
2025-12-13 21:20:35 +0000

    更新分流重写

--- script/app2smile.spotify.json.js
+++ script/app2smile.spotify.json.js
@@ -1,2 +1,2 @@
-// 2025/12/13 05:22:14 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.json.js
+// 2025/12/14 05:20:13 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.json.js
 let url=$request.url;url.includes("com:443")&&(url=url.replace(/com:443/,"com"));url.includes("platform=iphone")&&(url=url.replace(/platform=iphone/,"platform=ipad"));$done({url});
--- script/ddgksf2013.startup.iqiyi.js
+++ script/ddgksf2013.startup.iqiyi.js
@@ -1,2 +1,2 @@
-// 2025/12/13 05:22:14 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
+// 2025/12/14 05:20:13 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
 let obj=JSON.parse($response.body);delete obj.adSlots,$done({body:JSON.stringify(obj)});
