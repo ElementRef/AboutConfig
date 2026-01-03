773aad75138b432a8bf8e14599ff3e8062bc13ff
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-01-03 21:22:16 +0000

    更新分流重写

--- script/ddgksf2013.startup.jd.js
+++ script/ddgksf2013.startup.jd.js
@@ -1,2 +1,2 @@
-// 2026/1/3 05:19:24 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.jd.js
+// 2026/1/4 05:21:58 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.jd.js
 var puimCea1=JSON.parse($response.body);$request.url.indexOf("hotWords")!==-1&&(puimCea1.hotwords={},puimCea1.tabs={},delete puimCea1.abver);$request.url.indexOf("hotSearchTerms")!==-1&&(puimCea1.topData.data={},puimCea1.data={});$done({body:JSON.stringify(puimCea1)});
