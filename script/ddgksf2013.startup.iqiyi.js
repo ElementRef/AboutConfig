f46d7a38c82d8358099f03f00be72c9735ccb59e
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-01-10 21:22:07 +0000

    更新分流重写

--- script/ddgksf2013.startup.iqiyi.js
+++ script/ddgksf2013.startup.iqiyi.js
@@ -1,2 +1,2 @@
-// 2026/1/10 05:25:01 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
+// 2026/1/11 05:21:45 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
 let obj=JSON.parse($response.body);delete obj.adSlots,$done({body:JSON.stringify(obj)});
