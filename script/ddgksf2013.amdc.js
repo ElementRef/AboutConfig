5791bd5137aaeedbd359e4deb87e05d992515bbf
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-01-31 21:26:38 +0000

    更新分流重写

--- script/ddgksf2013.amdc.js
+++ script/ddgksf2013.amdc.js
@@ -1,2 +1,2 @@
-// 2026/1/31 05:32:16 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.amdc.js
+// 2026/2/1 05:26:20 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.amdc.js
 var ua=$request.headers["User-Agent"]||$request.headers["user-agent"];/(AMap|Cainiao|%E9%97%B2%E9%B1%BC|%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C|%E5%96%B5%E8%A1%97|%E5%A4%A9%E7%8C%AB|Alibaba|MovieApp|Hema4iPhone|Moon|DMPortal)/.test(ua)?$done({body:"ddgksf2013"}):$done({});
--- script/ddgksf2013.startup.iqiyi.js
+++ script/ddgksf2013.startup.iqiyi.js
@@ -1,2 +1,2 @@
-// 2026/1/31 05:32:16 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
+// 2026/2/1 05:26:20 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
 let obj=JSON.parse($response.body);delete obj.adSlots,$done({body:JSON.stringify(obj)});
