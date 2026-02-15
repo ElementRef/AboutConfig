3f976044619330c43cfe7591c85e3d5d26d7dc30
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-02-15 21:29:26 +0000

    更新分流重写

--- script/app2smile.spotify.json.js
+++ script/app2smile.spotify.json.js
@@ -1,2 +1,2 @@
-// 2026/2/15 05:29:02 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.json.js
+// 2026/2/16 05:29:14 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.json.js
 let url=$request.url;url.includes("com:443")&&(url=url.replace(/com:443/,"com"));url.includes("platform=iphone")&&(url=url.replace(/platform=iphone/,"platform=ipad"));$done({url});
--- script/ddgksf2013.startup.v2ex.js
+++ script/ddgksf2013.startup.v2ex.js
@@ -1,4 +1,4 @@
-// 2026/2/15 05:29:02 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
+// 2026/2/16 05:29:14 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
 var body=$response.body.replace(/<head>/,`<head>
     <style>
       .sidebar_units,
