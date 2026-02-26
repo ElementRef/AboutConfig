fefda1069a931767729ff2e777c6a6aaa816cc75
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-02-26 21:36:28 +0000

    更新分流重写

--- script/ddgksf2013.startup.v2ex.js
+++ script/ddgksf2013.startup.v2ex.js
@@ -1,4 +1,4 @@
-// 2026/2/26 05:38:48 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
+// 2026/2/27 05:36:05 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
 var body=$response.body.replace(/<head>/,`<head>
     <style>
       .sidebar_units,
