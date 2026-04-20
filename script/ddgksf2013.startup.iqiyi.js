70256dbd30b0e66fc7f9aa806c763bc8fed214f1
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-04-20 21:52:26 +0000

    更新分流重写

--- script/ddgksf2013.startup.iqiyi.js
+++ script/ddgksf2013.startup.iqiyi.js
@@ -1,2 +1,2 @@
-// 2026/4/20 05:40:18 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
+// 2026/4/21 05:52:03 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
 let obj=JSON.parse($response.body);delete obj.adSlots,$done({body:JSON.stringify(obj)});
--- script/ddgksf2013.startup.jd.js
+++ script/ddgksf2013.startup.jd.js
@@ -1,2 +1,2 @@
-// 2026/4/20 05:40:18 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.jd.js
+// 2026/4/21 05:52:03 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.jd.js
 var puimCea1=JSON.parse($response.body);$request.url.indexOf("hotWords")!==-1&&(puimCea1.hotwords={},puimCea1.tabs={},delete puimCea1.abver);$request.url.indexOf("hotSearchTerms")!==-1&&(puimCea1.topData.data={},puimCea1.data={});$done({body:JSON.stringify(puimCea1)});
