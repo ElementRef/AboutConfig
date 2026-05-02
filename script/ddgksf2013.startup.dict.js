a20f3ab6141a95dfec56b9ded6ea023f3259bb30
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-05-02 21:48:49 +0000

    更新分流重写

--- script/ddgksf2013.startup.dict.js
+++ script/ddgksf2013.startup.dict.js
@@ -1,2 +1,2 @@
-// 2026/5/2 05:56:16 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
+// 2026/5/3 05:48:32 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
 $done({body:"{}"});
--- script/ddgksf2013.startup.stay.js
+++ script/ddgksf2013.startup.stay.js
@@ -1,2 +1,2 @@
-// 2026/5/2 05:56:16 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.stay.js
+// 2026/5/3 05:48:32 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.stay.js
 let ddgksf2013=JSON.parse($response.body);ddgksf2013.biz&&(ddgksf2013.biz=Object.values(ddgksf2013.biz).filter(e=>e.type!="promoted"));$done({body:JSON.stringify(ddgksf2013)});
