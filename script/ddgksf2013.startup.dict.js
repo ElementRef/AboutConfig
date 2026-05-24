780990f60ccdf3ffb6a9d96c172ae3b69529007d
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-05-24 02:11:43 +0000

    更新分流重写

--- script/ddgksf2013.startup.dict.js
+++ script/ddgksf2013.startup.dict.js
@@ -1,2 +1,2 @@
-// 2026/5/24 05:57:32 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
+// 2026/5/24 10:11:19 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
 $done({body:"{}"});
--- script/ddgksf2013.startup.hang.lv.zong.heng.js
+++ script/ddgksf2013.startup.hang.lv.zong.heng.js
@@ -1,2 +1,2 @@
-// 2026/5/24 05:57:32 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.hang.lv.zong.heng.js
+// 2026/5/24 10:11:19 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.hang.lv.zong.heng.js
 var ua=$request.headers.rpid||$request.headers.Rpid;ua.includes("10000012")||ua.includes("1000019")?$done({status:"HTTP/1.1 404 Not Found"}):$done({});
