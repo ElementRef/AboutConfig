187922e672046cfdef4688c4d55194b243a642d6
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-05-30 22:03:28 +0000

    更新分流重写

--- script/ddgksf2013.startup.dict.js
+++ script/ddgksf2013.startup.dict.js
@@ -1,2 +1,2 @@
-// 2026/5/30 06:38:24 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
+// 2026/5/31 06:03:03 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
 $done({body:"{}"});
--- script/ddgksf2013.startup.zhihu.js
+++ script/ddgksf2013.startup.zhihu.js
@@ -1,2 +1,2 @@
-// 2026/5/30 06:38:24 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.zhihu.js
+// 2026/5/31 06:03:03 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.zhihu.js
 var body=$response.body.replace(/img_play_duration\\":\d+/g,'img_play_duration":0').replace(/launch_timeout\\":\d+/g,'launch_timeout":0');$done({body});
