38b86438a4eb2f1d25bb0ab7f8a66e96d2a996e1
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-06-07 22:08:09 +0000

    更新分流重写

--- script/ddgksf2013.startup.555.js
+++ script/ddgksf2013.startup.555.js
@@ -1,2 +1,2 @@
-// 2026/6/7 06:06:10 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.555.js
+// 2026/6/8 06:07:48 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.555.js
 let obj=JSON.parse($response.body);obj.data=obj.data.filter(t=>t.layout!=="advert_self"),obj.data.forEach(t=>{t.list=t.list.filter(a=>a.type!==3)}),$done({body:JSON.stringify(obj)});
--- script/ddgksf2013.startup.iqiyi.js
+++ script/ddgksf2013.startup.iqiyi.js
@@ -1,2 +1,2 @@
-// 2026/6/7 06:06:10 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
+// 2026/6/8 06:07:48 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
 let obj=JSON.parse($response.body);delete obj.adSlots,$done({body:JSON.stringify(obj)});
