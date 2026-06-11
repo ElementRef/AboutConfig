8288bfccbb85155c45ca8646fe1f24dae5cee046
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-06-11 22:51:00 +0000

    更新分流重写

--- script/ddgksf2013.startup.ithome.js
+++ script/ddgksf2013.startup.ithome.js
@@ -1,2 +1,2 @@
-// 2026/6/11 06:52:42 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.ithome.js
+// 2026/6/12 06:50:34 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.ithome.js
 var obj=JSON.parse($response.body);obj.data.list=Object.values(obj.data.list).filter(e=>e.feedContent.flag!=2),$done({body:JSON.stringify(obj)});
