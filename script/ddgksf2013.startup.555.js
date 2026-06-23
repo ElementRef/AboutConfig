8e199d30d44381cf6d4bcb120dab9203fff33d9f
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-06-23 22:17:22 +0000

    更新分流重写

--- script/ddgksf2013.startup.555.js
+++ script/ddgksf2013.startup.555.js
@@ -1,2 +1,2 @@
-// 2026/6/23 06:48:42 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.555.js
+// 2026/6/24 06:17:06 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.555.js
 let obj=JSON.parse($response.body);obj.data=obj.data.filter(t=>t.layout!=="advert_self"),obj.data.forEach(t=>{t.list=t.list.filter(a=>a.type!==3)}),$done({body:JSON.stringify(obj)});
--- script/ddgksf2013.startup.stay.js
+++ script/ddgksf2013.startup.stay.js
@@ -1,2 +1,2 @@
-// 2026/6/23 06:48:42 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.stay.js
+// 2026/6/24 06:17:06 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.stay.js
 let ddgksf2013=JSON.parse($response.body);ddgksf2013.biz&&(ddgksf2013.biz=Object.values(ddgksf2013.biz).filter(e=>e.type!="promoted"));$done({body:JSON.stringify(ddgksf2013)});
