36297e501fbd24b39cd5edf40ce13435805b51b5
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-07-26 05:57:11 +0000

    更新分流重写

--- script/ddgksf2013.startup.dict.js
+++ script/ddgksf2013.startup.dict.js
@@ -1,2 +1,2 @@
-// 2026/7/26 05:58:57 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
+// 2026/7/26 13:56:54 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
 $done({body:"{}"});
--- script/ddgksf2013.startup.ithome.js
+++ script/ddgksf2013.startup.ithome.js
@@ -1,2 +1,2 @@
-// 2026/7/26 05:58:57 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.ithome.js
+// 2026/7/26 13:56:54 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.ithome.js
 var obj=JSON.parse($response.body);obj.data.list=Object.values(obj.data.list).filter(e=>e.feedContent.flag!=2),$done({body:JSON.stringify(obj)});
