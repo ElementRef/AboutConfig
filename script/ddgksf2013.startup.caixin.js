7c4e904829ba04622720edba9f5521fa08edc1a7
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-07-29 07:21:24 +0000

    更新分流重写

--- script/ddgksf2013.startup.caixin.js
+++ script/ddgksf2013.startup.caixin.js
@@ -1,2 +1,2 @@
-// 2026/7/29 06:07:05 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.caixin.js
+// 2026/7/29 15:21:05 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.caixin.js
 var body=$response.body.replace(/sday":"[^"]*"/g,'sday":"2029-12-01 00:00:00"').replace(/eday":"[^"]*"/g,'eday":"2029-12-30 00:00:00"').replace(/intval":\d/g,'intval":0');$done({body});
--- script/ddgksf2013.startup.dict.js
+++ script/ddgksf2013.startup.dict.js
@@ -1,2 +1,2 @@
-// 2026/7/29 06:07:05 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
+// 2026/7/29 15:21:05 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
 $done({body:"{}"});
