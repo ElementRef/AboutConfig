b13c2022f3943bb2d22f1297b46143f6a03ea49b
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-08-04 22:16:03 +0000

    更新分流重写

--- script/app2smile.spotify.header.js
+++ script/app2smile.spotify.header.js
@@ -1,2 +1,2 @@
-// 2026/8/4 06:10:43 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.header.js
+// 2026/8/5 06:15:37 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.header.js
 let headers=$request.headers;delete headers["If-None-Match"];$done({headers});
--- script/ddgksf2013.startup.ithome.js
+++ script/ddgksf2013.startup.ithome.js
@@ -1,2 +1,2 @@
-// 2026/8/4 06:10:43 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.ithome.js
+// 2026/8/5 06:15:37 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.ithome.js
 var obj=JSON.parse($response.body);obj.data.list=Object.values(obj.data.list).filter(e=>e.feedContent.flag!=2),$done({body:JSON.stringify(obj)});
