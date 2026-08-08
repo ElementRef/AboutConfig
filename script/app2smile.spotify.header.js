e7bc22283863dce61b53d1fc70cf5a66b749df96
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-08-08 13:28:39 +0000

    更新分流重写

--- script/app2smile.spotify.header.js
+++ script/app2smile.spotify.header.js
@@ -1,2 +1,2 @@
-// 2026/8/8 20:41:56 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.header.js
+// 2026/8/8 21:28:10 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.header.js
 let headers=$request.headers;delete headers["If-None-Match"];$done({headers});
--- script/ddgksf2013.startup.iqiyi.js
+++ script/ddgksf2013.startup.iqiyi.js
@@ -1,2 +1,2 @@
-// 2026/8/8 20:41:56 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
+// 2026/8/8 21:28:11 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.iqiyi.js
 let obj=JSON.parse($response.body);delete obj.adSlots,$done({body:JSON.stringify(obj)});
