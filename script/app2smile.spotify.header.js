cf1a86a34ffd73ec8816998e84e72a4fbfb2daac
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-08-09 21:36:04 +0000

    更新分流重写

--- script/app2smile.spotify.header.js
+++ script/app2smile.spotify.header.js
@@ -1,2 +1,2 @@
-// 2026/8/9 05:33:24 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.header.js
+// 2026/8/10 05:35:43 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.header.js
 let headers=$request.headers;delete headers["If-None-Match"];$done({headers});
--- script/ddgksf2013.startup.dict.js
+++ script/ddgksf2013.startup.dict.js
@@ -1,2 +1,2 @@
-// 2026/8/9 05:33:24 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
+// 2026/8/10 05:35:44 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
 $done({body:"{}"});
