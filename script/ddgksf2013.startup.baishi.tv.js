9ff71bda70191c69cc2bfbc00138a1d2dcdf6b20
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-08-16 03:50:02 +0000

    更新分流重写

--- script/ddgksf2013.startup.baishi.tv.js
+++ script/ddgksf2013.startup.baishi.tv.js
@@ -1,2 +1,2 @@
-// 2026/8/16 05:23:52 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.baishi.tv.js
+// 2026/8/16 11:49:32 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.baishi.tv.js
 var obj=JSON.parse($response.body);obj.dt.data=Object.values(obj.dt.data).filter(e=>e.jumpTypeString.indexOf("广告")===-1),$done({body:JSON.stringify(obj)});
--- script/ddgksf2013.startup.dict.js
+++ script/ddgksf2013.startup.dict.js
@@ -1,2 +1,2 @@
-// 2026/8/16 05:23:52 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
+// 2026/8/16 11:49:32 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.dict.js
 $done({body:"{}"});
