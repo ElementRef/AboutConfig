1290c35fb0f73970bb095814b5bba53d58ec8577
ElementRef <168389155+ElementRef@users.noreply.github.com>
2026-08-17 21:28:55 +0000

    更新分流重写

--- script/app2smile.spotify.header.js
+++ script/app2smile.spotify.header.js
@@ -1,2 +1,2 @@
-// 2026/8/17 12:57:26 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.header.js
+// 2026/8/18 05:28:26 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/app2smile.spotify.header.js
 let headers=$request.headers;delete headers["If-None-Match"];$done({headers});
